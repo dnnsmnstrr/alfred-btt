08e8285a40b523c9c7aab8766131180015fcb497
Dennis Muensterer <dennismuensterer@aol.com>
2023-02-26 20:49:36 +0100

    Add try/catch to script filter

--- src/script-filter.js
+++ src/script-filter.js
@@ -1,25 +1,29 @@
 function run(argv) {
   var query = argv[0];
 
-  let BetterTouchTool = Application('BetterTouchTool');
-  let allNamedTriggersJSONString = BetterTouchTool.get_triggers({trigger_id: 643});
-  let allTriggers = JSON.parse(allNamedTriggersJSONString);
+  try {
+    let BetterTouchTool = Application('BetterTouchTool');
+    let namedTriggerId = 643
+    let allNamedTriggersJSONString = BetterTouchTool.get_triggers({trigger_id: namedTriggerId });
+    let allTriggers = JSON.parse(allNamedTriggersJSONString);
+    const items = allTriggers.map(trigger => {
+      const triggerUrl = `btt://trigger_named/?trigger_name=${encodeURIComponent(trigger.BTTTriggerName)}`
+      return {
+        uid: trigger.BTTUUID,
+        title: trigger.BTTTriggerName,
+        subtitle: trigger.BTTPredefinedActionName,
+        autocomplete: trigger.BTTTriggerName,
+        arg: trigger.BTTTriggerName,
+        text: {
+          copy: triggerUrl,
+          largetype: trigger.BTTGestureNotes || trigger.BTTGenericActionConfig
+        },
+        quicklookurl: triggerUrl
+      }
+    })
 
-  const items = allTriggers.map(trigger => {
-    const triggerUrl = `btt://trigger_named/?trigger_name=${encodeURIComponent(trigger.BTTTriggerName)}`
-    return {
-      uid: trigger.BTTUUID,
-      title: trigger.BTTTriggerName,
-      subtitle: trigger.BTTPredefinedActionName,
-      autocomplete: trigger.BTTTriggerName,
-      arg: trigger.BTTTriggerName,
-      text: {
-        copy: triggerUrl,
-        largetype: trigger.BTTGestureNotes || trigger.BTTGenericActionConfig
-      },
-      quicklookurl: triggerUrl
-    }
-  })
-
-  return JSON.stringify({ items });
+    return JSON.stringify({ items });
+  } catch (err) {
+    return JSON.stringify({ items: [] })
+  }
 }
